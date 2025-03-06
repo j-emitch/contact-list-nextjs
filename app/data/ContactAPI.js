965fe50f567ea7840bf92cfd6f0065abd93b2947
Joseph Mitchell <joseph.e.mitchell17@gmail.com>
2025-03-05 19:09:59 -0500

    feat: ContactApi

---
+++ app/data/ContactAPI.js
@@ -0,0 +1,20 @@
+export const ContactAPI = {
+	contacts: [
+		{ id: 1, name: 'Albert Einstein', image: 'https://upload.wikimedia.org/wikipedia/commons/1/14/Albert_Einstein_1947.jpg', email: 'aeinstein@example.com', phone: '15555555555' },
+
+	],
+	
+  all: function () {
+		return this.contacts;
+	},
+
+  addContact: function ({ name, image, email, phone }) {
+    const generateId = Math.round(Math.random() * 100000000);
+		this.contacts.push({ generateId, name, image, email, phone });
+	},
+
+	get: function (id) {
+		const isContact = (c) => c.id === id;
+		return this.contacts.find(isContact);
+	},
+};
