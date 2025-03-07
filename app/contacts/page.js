d78a5a1bccc382bea6fde2efff338311ed47b0ac
Joseph Mitchell <joseph.e.mitchell17@gmail.com>
2025-03-06 22:06:03 -0500

    add AllContactsHeader component

--- app/contacts/page.js
+++ app/contacts/page.js
@@ -2,33 +2,17 @@
 import { useState } from 'react';
 import { ContactAPI } from '../data/ContactAPI';
 import ContactList from '../components/ContactList';
-import Link from 'next/link';
-import Image from 'next/image';
+import AllContactsHeader from '../components/AllContactsHeader';
 
 export default function Contact() {
 	const [allContacts, _] = useState(ContactAPI.all());
 
 	  return (
-    <>
-      <div className='row'>
-        <div className='col-md-6 offset-md-3 mt-3 text-center'>
-          <div className='page-header'>
-            <h1>All Contacts</h1>
-          </div>
-
-          <Link href='/contacts/new'>
-            <button
-              type='button'
-              className='btn btn-primary add-contact'
-            >
-              Add Contact
-            </button>
-          </Link>
+      <>
+        <AllContactsHeader />
+        <div className='row'>
+          <ContactList contacts={allContacts} />
         </div>
-      </div>
-      <div className='row'>
-      <ContactList contacts={allContacts}/>
-      </div>
-    </>
+      </>
     );
   }
