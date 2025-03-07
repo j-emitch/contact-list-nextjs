9b71df3e38640ef67055988fa558ee1b99048b91
Joseph Mitchell <joseph.e.mitchell17@gmail.com>
2025-03-06 22:39:31 -0500

    change formatting

--- app/contacts/[id]/page.js
+++ app/contacts/[id]/page.js
@@ -4,17 +4,17 @@ import { useParams } from 'next/navigation';
 import { ContactAPI } from '../../data/ContactAPI';
 
 export default function Contact() {
-		const { id } = useParams();
+	const { id } = useParams();
 	const contact = ContactAPI.get(parseInt(id, 10));
 
 	if (!contact) {
-	return (
-		<>
-			<div className='text-center mt-4'>
-				<div>Sorry, but the contact was not found</div>
-				<Link href='/contacts' className='text-blue-500 hover:underline'>Back</Link>
-			</div>
-		</>
+		return (
+			<>
+				<div className='text-center mt-4'>
+					<div>Sorry, but the contact was not found</div>
+					<Link href='/contacts' className='text-blue-500 hover:underline'>Back</Link>
+				</div>
+			</>
 		);
 	}
 
@@ -24,7 +24,13 @@ export default function Contact() {
         <h1 className='text-2xl font-bold mt-4'>Contact Info</h1>
         <Link href='/contacts' className='text-blue-500 hover:underline'>Back</Link>
 				<h2 className='text-xl font-semibold mt-4'>{contact.name}</h2>
-        <img src={contact.image} alt={contact.name} width={500} height={500} className='w-24 h-24 rounded-full mx-auto mt-4'/>
+        <img 
+					src={contact.image}
+					alt={contact.name} 
+					width={500} 
+					height={500} 
+					className='w-24 h-24 rounded-full mx-auto mt-4'
+				/>
 				<h5 className='text-lg mt-2'>{contact.email}</h5>
 				<h5 className='text-lg'>{contact.phone}</h5>
 			</div>
